69c8f7b0f8cad487c56e78216811338834bb9d66
Adrian Loh <adrianloh@gmail.com>
2013-07-09 13:38:45 +0800

    D

--- framestore.js
+++ framestore.js
@@ -6,18 +6,36 @@ var Firebase = require("firebase"),
 	fs = require("fs"),
 	async = require("async");
 
-var machineBase = FramestoresBase.child(process.env.INSTANCE_ID);
+exec("curl -s http://169.254.169.254/latest/meta-data/instance-id",
+	function(error, stdout, stderr) {
+		var hostname;
+		if (stdout && stdout.match(/^i-/)) {
+			hostname = stdout;
+		} else {
+			hostname = process.env.HOSTNAME;
+		}
+		var machineBase = FramestoresBase.child(hostname);
+		machineBase.onDisconnect().remove();
+	});
 
-machineBase.onDisconnect().remove();
-
-var checkForMounted = setInterval(function() {
+var mounting = false,
+	checkForMounted = setInterval(function() {
+	if (mounting) { return; }
 	exec('sudo fdisk -l | grep /dev/md',
 		function (error, stdout, stderr) {
 			if (stdout.length>0) {
+				mounting = true;
 				var dev = "/dev/" + stdout.match(/(md\d+):/)[1],
 					target = "/media/framestore";
 				async.series([
-					function(stepDone) {
+					function checkSanityOfPaths(stepDone) {
+						if (dev.match(/\/dev\/md\d+/)) {
+							stepDone();
+						} else {
+							stepDone("Wonky device path: " + dev);
+						}
+					},
+					function checkMountPoint(stepDone) {
 						fs.exists(target, function (exists) {
 							if (!exists) {
 								fs.mkdirSync(target);
@@ -25,18 +43,23 @@ var checkForMounted = setInterval(function() {
 							stepDone();
 						});
 					},
-					function(stepDone) {
-						if (fs.readdirSync(target).length===0) {
-							exec("sudo mount -t xfs " + dev + " " + target, function(error, stdout, stderr) {
-								stepDone(error);
-							});
-						} else {
-							stepDone();
-						}
+					function mountFilesystem(stepDone) {
+						var cmd = "mount | grep " + target;
+						exec(cmd, function(error, stdout, stderr) {
+							if (stdout.length===0) {
+								cmd = "sudo mount -t xfs " + dev + " " + target;
+								console.log(cmd);
+								exec(cmd, function(error, stdout, stderr) {
+									stepDone(error);
+								});
+							} else {
+								stepDone("A filesystem is already mounted at " + target + " " + stdout);
+							}
+						});
 					}
 				], function onComplete(error) {
 					if (!error) {
-						clearTimeout(checkForMounted);
+						clearInterval(checkForMounted);
 						exec("df -h | grep md", function(error, stdout, stderr) {
 							if (stdout.length>0) {
 								var s = {},
@@ -49,8 +72,11 @@ var checkForMounted = setInterval(function() {
 								machineBase.set(s);
 							}
 						});
+					} else {
+						mounting = false;
+						console.log(error);
 					}
 				});
 			}
 		});
-}, 10000);
+}, 2500);
