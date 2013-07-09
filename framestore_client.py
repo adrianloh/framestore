7f565727dc205a552df381a28c6fbdc6932079f6
Adrian Loh <adrianloh@gmail.com>
2013-07-09 23:13:19 +0800

    python time

--- framestore_client.py
+++ framestore_client.py
@@ -7,6 +7,7 @@ import atexit
 
 base = "https://badabing.firebaseio-demo.com"
 framestoreBase = base + '/framestores.json'
+machine_id = os.popen("curl -s http://169.254.169.254/latest/meta-data/instance-id").read().strip()
 
 @atexit.register
 def removeBase():
@@ -28,32 +29,29 @@ while 1:
 	online = {}
 	if framestores:
 		for (instance_id, data) in framestores.items():
-			if data.has_key('status') and data.has_key('private_ip') and data.has_key('public_ip'):
+			if instance_id!=machine_id and data.has_key('status') and data.has_key('private_ip') and data.has_key('public_ip'):
 				status = data['status']
 				private_ip = data['private_ip']
 				public_ip = data['public_ip']
+				privateHostPath = private_ip + ":" + remoteMountPath
+				publicHostPath = public_ip + ":" + remoteMountPath
 				if status=='online' and data.has_key('mountedAt'):
 					remoteMountPath = data['mountedAt']
-					hostPath = private_ip + ":" + remoteMountPath
 					isMounted = os.popen("mount | grep " + remoteMountPath).read().strip()
 					if not isMounted:
 						if not os.path.exists(remoteMountPath):
 							os.mkdir(remoteMountPath)
-						mounted = mountNfs(hostPath)
+						mounted = mountNfs(privateHostPath)
 						if not mounted:
-							hostPath = public_ip + ":" + remoteMountPath
-							mountNfs(hostPath)
+							mountNfs(publicHostPath)
 					online[private_ip] = data
 					online[public_ip] = data
-				elif status=='offline':
-					[online.__delitem__(ip) for ip in (private_ip, public_ip) if online.has_key(ip)]
 
-	print online
 	mounted = [l.strip().split() for l in os.popen("mount").readlines() if re.search("media",l) and re.search("nfs",l)]
 	for mount in mounted:
-		host_ip = mount[0].split(":")[0]
+		hostPath = mount[0]
 		localMountPoint = mount[2]
-		if host_ip not in online.keys():
+		if hostPath not in online.keys():
 			cmd = "umount.nfs4 " + localMountPoint + " -fl"
 			os.popen(cmd).read().strip()
 
