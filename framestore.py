b308b3fdd2008723eb0866b1b3f3506602151210
Adrian Loh <adrianloh@gmail.com>
2013-07-09 15:57:11 +0800

    python time

---
+++ framestore.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env/python
+
+import os, re, json
+from time import sleep
+
+base = "https://badabing.firebaseio-demo.com"
+framestoreBase = base + '/framestores'
+
+instance_id = os.popen("curl -s http://169.254.169.254/latest/meta-data/instance-id").read().strip()
+public_ip = os.popen("curl -s http://169.254.169.254/latest/meta-data/public-ipv4").read().strip()
+private_ip = os.popen("curl -s http://169.254.169.254/latest/meta-data/local-ipv4").read().strip()
+hostname = os.popen("curl -s http://169.254.169.254/latest/meta-data/public-hostname").read().strip()
+machineBase = framestoreBase + '/' + instance_id
+stat = {
+	'public_ip': public_ip,
+	'private_ip': private_ip,
+	'hostname': hostname
+}
+cmd = """curl -sX PUT -d '%s' %s""" % (json.dumps(stat), machineBase + ".json")
+os.popen(cmd).read()
+
+
+def setData(key, value):
+	base = machineBase + "/" + key + ".json"
+	cmd = """curl -sX PUT -d '%s' %s""" % (json.dumps(value), base)
+	os.popen(cmd).read()
+
+
+def patchData(value):
+	base = machineBase + ".json"
+	cmd = """curl -sX PATCH -d '%s' %s""" % (json.dumps(value), base)
+	os.popen(cmd).read()
+
+
+def setStatus(status):
+	setData('status', status)
+
+
+while 1:
+	mountPath = "/media/framestore"
+	isMounted = os.popen("mount | grep " + mountPath).read().strip()
+	if isMounted:
+		setStatus("online")
+		res = os.popen("df -h | grep md").read().strip()
+		if res:
+			keys = ['devicePath', "available", "usedSpace", "freeSpace", "usedPercent", "mountedAt"]
+			h = dict(zip(keys, res.split()))
+			patchData(h)
+		sleep(10)
+	else:
+		raidReady = os.popen("sudo fdisk -l | grep /dev/md").read().strip()
+		if raidReady:
+			raidPath = re.findall("\/dev\/md\d+", raidReady)[0]
+			if not os.path.exists(mountPath):
+				os.mkdir(mountPath)
+			cmd = "sudo mount -t xfs " + raidPath + " " + mountPath;
+			setStatus("mounting")
+			os.popen(cmd).read().strip()
+		else:
+			setStatus("offline")
+			sleep(1)
--- framestore.sh
+++ framestore.sh
@@ -3,17 +3,17 @@
 # description: framestore notification service
 
 service_base=/tmp/service_framestore
-service_file=$service_base/framestore.js
+service_file=$service_base/framestore.py
 
 case $1 in
 	start)
 		[ -d $service_base ] && rm -R $service_base
         /usr/bin/git clone https://github.com/adrianloh/framestore.git $service_base
-		/usr/local/bin/node $service_file &
+		/usr/bin/python $service_file &
 		;;
 	restart)
 		kill -9 `ps ax | grep $service_file | grep -v grep | awk '{print $1}'`
-		/usr/local/bin/node $service_file
+		/usr/bin/python $service_file
 		;;
 	status)
 		proc=`ps ax | grep $service_file | grep -v grep | awk '{print $1}'`
