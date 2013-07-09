3b3e6034b34fab21020e170d35cd096b6e88d0e2
Adrian Loh <adrianloh@gmail.com>
2013-07-09 21:26:26 +0800

    python time

--- framestore_client.py
+++ framestore_client.py
@@ -12,35 +12,49 @@ framestoreBase = base + '/framestores.json'
 def removeBase():
 	pass
 
+def mountNfs(hostPath):
+	cmd = "mount.nfs4 " + hostPath + " " + remoteMountPath
+	proc = Popen(cmd, shell=True)
+	sleep(10)
+	if proc.poll() is None:
+		proc.kill()
+		return False
+	else:
+		return True
+
+
+online = {}
 while 1:
 	framestores = json.loads(os.popen("curl -s %s" % framestoreBase).read().strip())
-	online = []
 	if framestores:
 		for (instance_id, data) in framestores.items():
-			if data.has_key('status') \
-				and (data['status']=='online') \
-				and data.has_key('mountedAt') \
-				and data.has_key('private_ip') \
-				and data.has_key('public_ip'):
-				remoteMountPath = data['mountedAt']
-				ip = data['private_ip']
-				isMounted = os.popen("mount | grep " + remoteMountPath).read().strip()
-				hostPath = ip + ":" + remoteMountPath
-				if not isMounted:
-					if not os.path.exists(remoteMountPath):
-						os.mkdir(remoteMountPath)
-					cmd = "mount -t nfs " + hostPath + " " + remoteMountPath
-					proc = Popen(cmd, shell=True)
-					sleep(10)
-					if proc.poll() is None:
-						proc.kill()
-				online.append(hostPath)
+			if data.has_key('status') and data.has_key('private_ip') and data.has_key('public_ip'):
+				status = data['status']
+				private_ip = data['private_ip']
+				public_ip = data['public_ip']
+				if status=='online' and data.has_key('mountedAt'):
+					remoteMountPath = data['mountedAt']
+					hostPath = private_ip + ":" + remoteMountPath
+					isMounted = os.popen("mount | grep " + remoteMountPath).read().strip()
+					if not isMounted:
+						if not os.path.exists(remoteMountPath):
+							os.mkdir(remoteMountPath)
+						mounted = mountNfs(hostPath)
+						if not mounted:
+							hostPath = public_ip + ":" + remoteMountPath
+							mountNfs(hostPath)
+					online[private_ip] = data
+					online[public_ip] = data
+				elif status=='offline':
+					[online.__delitem__(ip) for ip in (private_ip, public_ip) if online.has_key(ip)]
+	else:
+		online = {}
 
 	mounted = [l.strip().split() for l in os.popen("mount").readlines() if re.search("media",l) and re.search("nfs",l)]
 	for mount in mounted:
-		hostPath = mount[0]
+		host_ip = mount[0].split(":")[0]
 		localMountPoint = mount[2]
-		if hostPath not in online:
+		if host_ip not in online.keys():
 			cmd = "umount.nfs4 " + localMountPoint + " -fl"
 			os.popen(cmd).read().strip()
 
