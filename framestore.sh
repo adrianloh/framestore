4ca0e22b13634de840798a4f1e8eb11e21ad9293
Adrian Loh <adrianloh@gmail.com>
2013-07-09 13:42:49 +0800

    D

---
+++ framestore.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# chkconfig: 345 30 01
+# description: framestore notification service
+
+service_base=/tmp/service_framestore
+service_file=$service_base/framestore.js
+
+case $1 in
+	start)
+		[ -d $service_base ] && rm -R $service_base
+        /usr/bin/git clone https://github.com/adrianloh/framestore.git $service_base
+		/usr/local/bin/node $service_file &
+		;;
+	restart)
+		kill -9 `ps ax | grep $service_file | grep -v grep | awk '{print $1}'`
+		/usr/local/bin/node $service_file
+		;;
+	status)
+		proc=`ps ax | grep $service_file | grep -v grep | awk '{print $1}'`
+		if [ -n "$proc" ]; then
+			echo Framestore is running \($proc\)...
+		else
+			echo Framestore is stopped
+		fi
+		;;
+    stop)
+		kill -9 `ps ax | grep $service_file | grep -v grep | awk '{print $1}'`
+		rm -R $service_base
+		echo Framestore is stopped
+		;;
+esac
